26b094af8996e7ea48d6998ea79587d8f9c6f224
Matt Russo <matthew.russo91@gmail.com>
2025-09-29 20:48:29 -0400

    add conditional nav links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 import { Link } from '@tanstack/react-router';
 import logo from '../assets/logos/logo-project-earth-health.png';
+import { useAuth } from '@/context/AuthContext';
 
 const Header = () => {
+  const { user } = useAuth();
+  
   return (
     <header className="bg-primary--dark pt">
       <div className="container navbar grid-2">
@@ -17,24 +20,34 @@ const Header = () => {
           >
             Cleanups
           </Link>
-          <Link
-            to='/cleanups/new'
-          >
-            + New Cleanup
-          </Link>
+          {user && (
+            <Link to='/cleanups/new'>+ New Cleanup</Link>
+          )}
           <Link to="/about">
             About
           </Link>
-          <Link to="/profile">
-            My Profile
-          </Link>
-          <Link to="/signup">
-            <button className="btn btn-primary">Sign Up</button>
-          </Link>
-          <Link to="/login">
-            <button className="btn">Log In</button>
-          </Link>
-          
+          {user && (
+            <Link to='/profile'>My Profile</Link>
+          )}
+          {
+            !user ? (
+              <>
+                <Link to="/signup">
+                  <button className="btn btn-primary">Sign Up</button>
+                </Link>
+                <Link to="/login">
+                  <button className="btn">Log In</button>
+                </Link>
+              </>
+            ) : (
+              <>
+                {/* <span>Welcome, { user.username }</span> */}
+                <Link to="/login">
+                  <button className="btn btn-danger">Log Out</button>
+                </Link>
+              </>
+            )
+          }
         </div>
       </div>
     </header>
