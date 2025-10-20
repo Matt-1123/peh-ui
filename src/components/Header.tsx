5ea8389f80d0885c89ef3a32b366d088ac3e761c
Matt Russo <matthew.russo91@gmail.com>
2025-10-19 20:36:12 -0400

    default submenu to closed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const navigate = useNavigate();
   const { user, setUser, setAccessToken } = useAuth();
 
-  const [openSubmenu, setOpenSubmenu] = useState(true);
+  const [openSubmenu, setOpenSubmenu] = useState(false);
 
   const handleMouseEnter = () => {
     setOpenSubmenu(true);
