8243f4fb0fab5ad608311d76a62f77a48795bdb0
Matt Russo <matthew.russo91@gmail.com>
2025-10-25 20:08:17 -0400

    add plastic swap action to mobile nav menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -15,14 +15,24 @@ const MobileMenu = ({ openMobileMenu, user, handleLogout }) => {
                 <Link to="/" className="nav-link">Home</Link>
             </li>
             <li>
-                <span className="nav-link">Cleanups</span>
+                <span className="nav-link">Actions</span>
                 <ul className='ml-1'>
                     <li>
                         <Link to='/cleanups' activeOptions={{ exact: true }}>All Cleanups</Link>
                     </li>
                     {user && (
                         <li>
-                        <Link to='/cleanups/new'>+ New Cleanup</Link>
+                            <Link to='/cleanups/new'>+ New Cleanup</Link>
+                        </li>
+                    )}
+                    <li>
+                        {/* <Link to='/plastic-swaps' activeOptions={{ exact: true }}>All Plastic Swaps</Link> */}
+                        <p style={{ lineHeight: "1.2" }}>All Plastic Swaps (Coming Soon)</p>
+                    </li>
+                    {user && (
+                        <li>
+                            {/* <Link to='/plastic-swaps/new'>+ New Plastic Swap</Link> */}
+                            <p style={{ lineHeight: "1.2" }}>+ New Plastic Swap (Coming Soon)</p>
                         </li>
                     )}
                 </ul>
