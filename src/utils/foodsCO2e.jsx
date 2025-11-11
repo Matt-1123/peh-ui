e21cf2a22f7106ac30808ea03a43ba2b2dc9534e
Matt Russo <matthew.russo91@gmail.com>
2025-11-10 21:28:22 -0500

    add avoided foods data (name and kg CO2e per kg)

---
+++ src/utils/foodsCO2e.jsx
@@ -0,0 +1,60 @@
+const foodsCO2e = [
+  {
+    name: "Beef (beef herd)",
+    kgCO2ePerKg: 60
+  },
+  {
+    name: "Lamb",
+    kgCO2ePerKg: 24
+  },
+  {
+    name: "Mutton",
+    kgCO2ePerKg: 24
+  },
+  {
+    name: "Cheese",
+    kgCO2ePerKg: 21
+  },
+  {
+    name: "Beef (dairy herd)",
+    kgCO2ePerKg: 21
+  },
+  {
+    name: "Chocolate",
+    kgCO2ePerKg: 19
+  },
+  {
+    name: "Coffee",
+    kgCO2ePerKg: 17
+  },
+  {
+    name: "Prawns (farmed)",
+    kgCO2ePerKg: 12
+  },
+  {
+    name: "Pork",
+    kgCO2ePerKg: 7
+  },
+  {
+    name: "Poultry (chicken, turkey, duck, goose)",
+    kgCO2ePerKg: 6
+  },
+  {
+    name: "Fish (farmed)",
+    kgCO2ePerKg: 5
+  },
+  {
+    name: "Eggs",
+    kgCO2ePerKg: 4.5
+  },
+  {
+    name: "Rice",
+    kgCO2ePerKg: 4
+  },
+  {
+    name: "Milk (cow)",
+    kgCO2ePerKg: 3
+  }
+];
+
+export default foodsCO2e;
