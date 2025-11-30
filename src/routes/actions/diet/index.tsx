c7fa50b52f6363f2bbdfc33b4ebe5b2e5325653f
Matt Russo <matthew.russo91@gmail.com>
2025-11-30 15:42:12 -0500

    add fetchDietActionMeals

--- src/routes/actions/diet/index.tsx
+++ src/routes/actions/diet/index.tsx
@@ -1,9 +1,31 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {queryOptions, useSuspenseQuery} from '@tanstack/react-query'
+import { fetchDietActionMeals } from '@/api/dietActions'
+const dietActionsQueryOptions = () => queryOptions({
+  queryKey: ['dietActions'],
+  queryFn: () => fetchDietActionMeals(),
+})
 
 export const Route = createFileRoute('/actions/diet/')({
+  head: () => ({
+    meta: [
+      {
+        title: 'PEH - Diet Actions'
+      }
+    ]
+  }),
   component: RouteComponent,
+  loader: async ({ context: { queryClient } }) => {
+    return queryClient.ensureQueryData(dietActionsQueryOptions())
+  }
 })
 
 function RouteComponent() {
-  return <div>Hello "/actions/diet/"!</div>
+  const { data: dietActions } = useSuspenseQuery(dietActionsQueryOptions())
+  return <>
+    <h1>All Diet Actions</h1>
+    {dietActions.map(action => (
+      <p>{action.mealName}</p>
+    ))}
+  </>
 }
