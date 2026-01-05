98a55613df335d44a50a5b67b9224ffd48b93805
Matt Russo <matthew.russo91@gmail.com>
2026-01-04 20:40:41 -0500

    add loading Spinner to All Cleanups page

--- src/routes/actions/cleanups/index.tsx
+++ src/routes/actions/cleanups/index.tsx
@@ -1,7 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
-import {queryOptions, useSuspenseQuery} from '@tanstack/react-query'
 import { fetchCleanups } from '@/api/cleanups'
 import CleanupFeedItem from '@/components/CleanupFeedItem'
+import Spinner from '@/components/Spinner'
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query'
+import { createFileRoute } from '@tanstack/react-router'
+
 const cleanupsQueryOptions = () => queryOptions({
   queryKey: ['cleanups'],
   queryFn: () => fetchCleanups(),
@@ -18,14 +20,17 @@ export const Route = createFileRoute('/actions/cleanups/')({
   component: CleanupsPage,
   loader: async ({ context: { queryClient } }) => {
     return queryClient.ensureQueryData(cleanupsQueryOptions())
-  }
+  },
+  pendingComponent: () => (
+    <div className="container-narrow">
+      <h1 className='mb-3'>All Cleanups</h1>
+      <Spinner />
+    </div>
+  )
 })
 
 function CleanupsPage() {
   const { data: cleanups } = useSuspenseQuery(cleanupsQueryOptions())
-  // const cleanups = [...data].sort(
-  //   (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-  // )
   
   return <>
     <div className="container-narrow">
@@ -33,6 +38,6 @@ function CleanupsPage() {
       {cleanups.map(cleanup => (
         <CleanupFeedItem key={cleanup.id} cleanup={cleanup} />
       ))}
-    </div>
+    </div> 
   </>
 }
